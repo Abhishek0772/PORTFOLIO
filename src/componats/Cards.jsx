fbb7f57bbd39304bef6d3cf43be31520f85e8b6a
abhi <satputeabhishek2888@gmail.com>
2022-11-04 02:59:17 -0700

    card

--- src/componats/Cards.jsx
+++ src/componats/Cards.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import Bike from '../img/bike.jpg'
 function Cards() {
   return (
-    <div className='flex justify-center items-center'>
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div className='flex justify-center items-center '>
+      <div className='flex justify-start items-center flex-col w-80 h-[480px] shadow-sm shadow-gray-500 bg-slate-300 bg-opacity-40 backdrop-blur-3xl rounded-md'>
+        <div className='w-full  flex justify-center items-start'>
+          <img className='w-72 h-72 rounded-md' src={Bike} alt="" />
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+          <h1 className='font-bold my-2 text-xl'>BIKE</h1>
+          <p className='text-center px-2 py-2 text-gray-800'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, totam?
+          </p>
+          <button className='backdrop-blur-3xl shadow-xl shadow-slate-400 bg-red-400 bg-opacity-40 p-1 px-4 rounded-md my-1'>See</button>
         </div>
 
       </div>
--- src/componats/Hero.jsx
+++ src/componats/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <>
     <section className='relative h-full'>
       <div className='absolute top-0 flex-wrap filter bg-gray-300 flex justify-center items-center gap-2 -z-10 container mx-auto w-full h-full backdrop-blur-3xl blur-3xl'>
-          <div className='h-60 w-60 bg-purple-600 border-3 border-green-400 animate-pulse transform -translate-y-40 rounded-full' ></div>
+          <div className='h-60 w-60 bg-purple-600 border-3 border-green-400 transform -translate-y-40 rounded-full' ></div>
           <div className='h-60 w-60 bg-yellow-400 border-8 border-green-500' ></div>
           <div className='h-60 w-60 bg-red-400' ></div>
       </div>
