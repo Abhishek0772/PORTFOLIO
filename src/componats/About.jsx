ac9be04fc79f4292ddc397f0472989669e5aa08e
abhi <satputeabhishek2888@gmail.com>
2022-11-08 00:08:24 -0800

    a

--- src/componats/About.jsx
+++ src/componats/About.jsx
@@ -8,10 +8,10 @@ function About() {
         <div className='relative h-screen'>
         
         <div className='w-full bg-slate-300 bg-opacity-30 flex justify-center items-center flex-wrap p-2'>
-            <div className='w-[300px] h-[400px] p-5 shadow-md shadow-black'>
+            <div className='w-[300px] h-[400px] p-5'>
                 <img className='w-full h-full  ' src={abhi} alt="" />
             </div>
-            <div className='w-1/2'>
+            <div className='w-1/2 px-2 flex justify-start flex-col'>
                 <h3 className='text-3xl font-bold '>About Me</h3>
                 <span>developer & designer </span>
                 <p className=''>I am a front-end developer. i can proved you a clean code and pixel perfect design. i also make the website more and more interactive with web animation. A responsive design make your website accessible to all users. regardless of their devices </p>
