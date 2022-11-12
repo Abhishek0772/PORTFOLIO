dfb3cc82ab0159739ec9bc36e5e46e2e762ddf98
abhi <satputeabhishek2888@gmail.com>
2022-11-12 02:27:18 -0800

    a

--- src/componats/Blur.jsx
+++ src/componats/Blur.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Blur({val}) {
   return (
-    <div className={`absolute top-0 flex-wrap filter bg-gray-300 flex justify-center  gap-4 transform translate-y-20 -z-10 container mx-auto w-full h-full ${val}  overflow-hidden blur-2xl`}>
+    <div className={`absolute top-0 flex-wrap filter bg-gray-300 flex justify-center  gap-4 transform translate-y-20 -z-10 container mx-auto w-full h-full ${val}  overflow-hidden blur-3xl`}>
     <div className='animate-purple  h-60 w-60 bg-purple-600 border-3 border-green-400  rounded-full' ></div>
-    <div className='animate-pulse h-60 w-60 bg-yellow-500 border-8 rounded-full border-green-500 transform translate-y-32' ></div>
+    <div className='animate-pulse h-60 w-60 bg-yellow-500 border-8 rounded-full border-green-500 transform md:translate-y-32' ></div>
     <div className='animate-pink h-60 w-60 bg-red-400  rounded-full' ></div>
 </div>
   )
