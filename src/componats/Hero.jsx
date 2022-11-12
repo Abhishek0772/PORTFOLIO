ea7ae6f4cb95036d12ad7c069fa8f1852d8244d6
abhi <satputeabhishek2888@gmail.com>
2022-11-12 02:41:41 -0800

    a

--- src/componats/Hero.jsx
+++ src/componats/Hero.jsx
@@ -4,8 +4,8 @@ import Blur from './Blur'
 function Hero() {
   return (
     <>
-    <section className='relative h-screen w-full flex justify-center items-start mt-14 flex-wrap gap-5 border-2 border-red-600' >
-<div className='shadow-md shadow-gray-100 h-4/5 flex flex-col gap-5 justify-center w-11/12 items-center'>
+    <section className='relative h-screen w-full flex justify-center items-start mt-14 flex-wrap gap-5' >
+<div className=' h-4/5 flex flex-col gap-5 justify-center w-11/12 items-center'>
   <h1 className='text-5xl font-bold text-center -mt-10  '>Web Developer</h1>
   <p className='text-md opacity-80 w-4/5 text-center'>
   I Am frontend developer with high level of skills in web designing and development , producing the quality work
--- src/componats/Navbar.jsx
+++ src/componats/Navbar.jsx
@@ -28,18 +28,18 @@ function Navbar() {
               <Link className='cursor-pointer' to='projects'offset={-70} duration={800} smooth={true}><li>PROJECTS</li></Link>
             </ul>
           </div>
-          <div className='md:hidden' onClick={clicked}>
-            <div className='bg-black h-1 w-6 my-1  rounded-sm'></div>
-            <div className='bg-black h-1 w-6 my-1  rounded-sm'></div>
-            <div className='bg-black h-1 w-6 my-1  rounded-sm'></div>
+          <div className='md:hidden w-8 flex flex-col items-end justify-end gap-1' onClick={clicked}>
+            <div className='bg-black h-1 w-7   rounded-sm'></div>
+            <div className='bg-black h-1 w-5  rounded-sm'></div>
+            <div className='bg-black h-1 w-4   rounded-sm'></div>
           </div>
         </div >
         <div id='menu'>
           <ul className={` bg-slate-200 bg-opacity-40 flex flex-col items-start px-3 fixed w-full md:hidden gap-2 py-2 z-20 transition duration-1000 ${click ? 'left-100' : '-left-full'} `}>
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>CONTACT</li>
-            <li>PROJECTS</li>
+          <Link to='home' duration={800} offset={-70} smooth={true} className='cursor-pointer'><li>HOME</li></Link>
+              <Link className='cursor-pointer' to='about' duration={800} smooth={true} offset={-70}><li>ABOUT</li></Link>
+              <Link className='cursor-pointer' to='home' duration={800} smooth={true}><li>CONTACT</li></Link>
+              <Link className='cursor-pointer' to='projects'offset={-70} duration={800} smooth={true}><li>PROJECTS</li></Link>
           </ul>
         </div>
       </nav>
