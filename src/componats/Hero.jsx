526cced7c91e3a6a957357cfe1a581284e7ef834
abhi <satputeabhishek2888@gmail.com>
2022-11-12 22:18:46 -0800

    a

--- src/componats/Hero.jsx
+++ src/componats/Hero.jsx
@@ -4,11 +4,11 @@ import Blur from './Blur'
 function Hero() {
   return (
     <>
-    <section className='relative h-screen w-full flex justify-center items-start mt-12  flex-wrap ' >
-      <div className='h-2/3 w-2/3 rounded-lg backdrop-blur-2xl flex justify-center items-center  flex-wrap gap-5'>
+    <section className='relative h-screen w-full flex justify-center items-start  flex-wrap gap-5 ' >
+      <div className='h-full w-full backdrop-blur-2xl flex justify-center items-center'>
 
-<div className=' h-4/5 flex flex-col justify-center w-11/12 items-center '>
-  <h1 className='text-6xl font-bold text-center '>Web <span className='text-3xl md:text-6xl sm:text-6xl'>Developer</span></h1>
+<div className=' h-4/5 flex flex-col gap-5 justify-center w-11/12 items-center '>
+  <h1 className='text-6xl font-bold text-center -mt-10 '>Web Developer</h1>
   <p className='text-md opacity-80 w-4/5 text-center'>
   I Am frontend developer with high level of skills in web designing and development , producing the quality work
   </p>
@@ -18,7 +18,7 @@ function Hero() {
 </div>
       </div>
     </section>
-<Blur val='items-center'/>
+<Blur val='items-start'/>
     
     </>
   )
--- src/componats/Skills.jsx
+++ src/componats/Skills.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Blur from './Blur'
-
+import web1 from '../img/html5.png'
+import web2 from '../img/css.png'
+import web3 from '../img/js.png'
 function Skills() {
   return (
     <section className='relative'>
-        <div>
-          <img src="" alt="" />
+      <div className='w-full flex justify-center items-center'>
+
+        <div className='w-12 h-12'>
+          <img className='w-full h-full object-cover' src={web1} alt="" />
         </div>
-        <div></div>
-        <div></div>
+        <div className='w-12 h-12'><img  className='w-full h-full object-cover' src={web2} alt="" /></div>
+        <div className='w-12 h-12'><img  className='w-full h-full object-cover'  src={web3} alt="" /></div>
+      </div>
         <Blur val='items-center'/>
     </section>
   )
