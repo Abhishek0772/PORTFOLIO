7dcd92a47370e9c402bc89095d0a4d8c3b6de434
abhi <abhisheksatpute2668@gmail.com>
2023-07-08 09:42:05 -0700

    first commit

--- src/componats/Blur.jsx
+++ src/componats/Blur.jsx
@@ -5,7 +5,7 @@ function Blur({val}) {
     <div className={`absolute top-0 flex-wrap backdrop-blur-3xl bg-gray-100 flex justify-center gap-4 -z-10 container mx-auto w-full h-full ${val} overflow-hidden  bg-blend-overlay blur-xl`}>
     <div className='animate-purple bg-blend-screen  h-52 w-52 md:w-60 bg-purple-600 border-3 border-green-400  rounded-full' ></div>
     <div className='animate-yellow h-60 w-52 md:w-60 bg-yellow-500 rounded-full '></div>
-    <div className='animate-blue h-52 w-52 md:w-60 bg-blue-600  rounded-full' ></div>
+    <div className='animate-pink h-52 w-52 md:w-60 bg-blue-600  rounded-full' ></div>
 </div>
   )
 }
